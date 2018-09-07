2701ecb27e1c850255fb96f0d070cfa8d48ee87f
sashalospalluto <36174200+sashalospalluto@users.noreply.github.com>
2018-09-07 00:35:36 -0300

    Delete funciones..c

--- funciones..c
+++
@@ -1,8 +0,0 @@
-
-    float funcionSuma(float a,float b); //Realiza la suma
-    float funcionResta (float a,float b); //Realiza la resta
-    float funcionMultiplicar (float a, float b); //Realiza la multiplicacion
-    float funcionDividir(float a, float b); //Realiza la division
-    float funcionFactorial(float a); //Realiza el factoreal de un numero
-    void respuesta (char mensaje[], float resultado); //Muestra el resultado de la operacion
-
